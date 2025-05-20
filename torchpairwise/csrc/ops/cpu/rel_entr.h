0ff57b08fdfbc2b76ba801e9363b760167767e81
Hoang-Nhat Tran <hnhat.tran@gmail.com>
2025-05-20 10:10:35 +0200

    fix isnan

--- torchpairwise/csrc/ops/cpu/rel_entr.h
+++ torchpairwise/csrc/ops/cpu/rel_entr.h
@@ -7,7 +7,7 @@ namespace torchpairwise {
     namespace ops {
         template<typename T>
         __forceinline__ constexpr T rel_entr(const T &x, const T &y) {
-            if (isnan(x))
+            if (std::isnan(x))
                 return x;
             else if (x > T(0) && y > T(0))
                 return x * log(x / y);
